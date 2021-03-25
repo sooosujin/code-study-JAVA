d1ac909c5e408f369517409e2759726e301ada52
sooosujin <71065377+sooosujin@users.noreply.github.com>
2021-03-25 15:15:22 +0900

    Add files via upload
    JAVA study Day2

---
+++ StringApp.java
@@ -0,0 +1,21 @@
+
+public class StringApp {
+
+	public static void main(String[] args) {
+	
+		System.out.println("Hello World"); // String(문자열)
+		System.out.println('H'); // Character(문자)
+		System.out.println("H");
+		
+		System.out.println("Hello "
+				+ "World");
+		
+		// new line
+		System.out.println("Hello \nWorld");
+		
+		// escape
+		System.out.println("Hello \"World\"");// Hello "World"
+		
+	}
+
+}
---
+++ StringOperation.java
@@ -0,0 +1,11 @@
+
+public class StringOperation {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World".length()); // 11
+		System.out.println("Hello, sujin . . . bye.".replace("sujin","egoing"));
+
+	}
+
+}
